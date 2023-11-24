99c07d2a14c434bad28af121673563358df8e9c5
w69788 <w69788@student.wsiz.edu.pl>
2023-11-24 20:29:54 +0100

    aaa

---
+++ lab0.1.py
@@ -0,0 +1,5 @@
+a = float(input("Podaj długość pierwszego boku prostokąta w cm "))
+b = float(input("Podaj długość drugiego boku prostokąta w cm "))
+p = a * b
+g = 2 * a + 2 * b
+print(f"Pole prostokąta wynosi {p}cm^2 \nObwód prostokąta wynosi {g}cm")
